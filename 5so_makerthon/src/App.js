69833a1a93bf3201e6684b1a423113eafcf60c7c
changuniuni <changchang@unist.ac.kr>
2021-09-14 14:05:55 +0900

    프론트 1차완성

---
+++ 5so_makerthon/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {createGlobalStyle} from 'styled-components';
+import MainContext from "./components/MainContext.js";
+
+const GlobalStyle=createGlobalStyle`
+body{
+  background:#536349;
+
+  p {
+    font-family:"NanumSquareRoundL";
+    font-size:11px;
+    float:right;
+    margin-bottom:0px;
+  }
+}
+`;
+
+
+function App() {
+  return(
+    <>
+    <GlobalStyle />
+    <div><MainContext /></div>
+    <p>Copyright (c) 2021 이승엽,
+      김창현, 이준용, 한준서, 백창인</p>
+    </>
+  )
+}
+
+export default App;
